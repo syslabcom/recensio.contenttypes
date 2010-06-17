fe280b4627d5091893759a14d2108b7ace3281c6
deroiste <deroiste@51ee9ba4-e84d-4896-84de-354f1821c169>
2010-06-17 14:18:02 +0000

    Adding basic rezension schema fields
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.contenttypes/trunk@5268 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/contenttypes/content/rezension.py
+++ recensio/contenttypes/content/rezension.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """Definition of the Rezension content type
 """
 
@@ -7,15 +8,138 @@ from Products.Archetypes import atapi
 from Products.ATContentTypes.content import base
 from Products.ATContentTypes.content import schemata
 
-# -*- Message Factory Imported Here -*-
-
+from recensio.contenttypes import contenttypesMessageFactory as _
 from recensio.contenttypes.interfaces import IRezension
 from recensio.contenttypes.config import PROJECTNAME
 
 RezensionSchema = schemata.ATContentTypeSchema.copy() + atapi.Schema((
-
-    # -*- Your Archetypes field definitions here ... -*-
-
+    atapi.LinesField(
+        'rezensionType',
+        storage=atapi.AnnotationStorage(),
+        required=True,
+        vocabulary=("Monographie", "Zeitscrift"),
+        widget=atapi.SelectionWidget(
+            label=_(u"Rezension Type"),
+            description=_(u"Rezension einer Monographie, Zeitscrift, usw."),
+            format="select",
+        ),
+    ),
+    atapi.StringField(
+        'rezensionAutor',
+        storage=atapi.AnnotationStorage(),
+        widget=atapi.StringWidget(
+            label=_(u"Autor der Rezension"),
+        ),
+    ),
+    atapi.StringField(
+        'praesentiertenScriftTextsprache',
+        storage=atapi.AnnotationStorage(),
+        widget=atapi.StringWidget(
+            label=_(u"Textsprache der präsentierten Schrift"),
+        ),
+    ),
+    atapi.StringField(
+        'praesentationTextsprache',
+        storage=atapi.AnnotationStorage(),
+        widget=atapi.StringWidget(
+            label=_(u"Textsprache der Präsentation"),
+        ),
+    ),
+    atapi.StringField(
+        'recensioID',
+        storage=atapi.AnnotationStorage(),
+        widget=atapi.StringWidget(
+            label=_(u"Recensio ID"),
+        ),
+    ),
+    # May have multiple authors
+    atapi.LinesField(
+        'autorDesBuchs',
+        storage=atapi.AnnotationStorage(),
+        widget=atapi.LinesWidget(
+            label=_(u"Author des Buchs"),
+            rows=3,
+        ),
+    ),
+    atapi.StringField(
+        'titel',
+        storage=atapi.AnnotationStorage(),
+        widget=atapi.StringWidget(
+            label=_(u"Titel"),
+        ),
+    ),
+    atapi.StringField(
+        'untertitel',
+        storage=atapi.AnnotationStorage(),
+        widget=atapi.StringWidget(
+            label=_(u"Untertitel"),
+        ),
+    ),
+    # Q: DateTimeField or perhaps IntegerField ?
+    atapi.StringField(
+        'erscheinungsjahr',
+        storage=atapi.AnnotationStorage(),
+        widget=atapi.StringWidget(
+            label=_(u"Erscheinungsjahr"),
+        ),
+    ),
+    atapi.StringField(
+        'erscheinungsort',
+        storage=atapi.AnnotationStorage(),
+        widget=atapi.StringWidget(
+            label=_(u"Erscheinungsort"),
+        ),
+    ),
+    atapi.StringField(
+        'verlag',
+        storage=atapi.AnnotationStorage(),
+        widget=atapi.StringWidget(
+            label=_(u"Verlag"),
+        ),
+    ),
+    atapi.StringField(
+        'reihe',
+        storage=atapi.AnnotationStorage(),
+        widget=atapi.StringWidget(
+            label=_(u"Reihe"),
+        ),
+    ),
+    atapi.StringField(
+        'reihennummer',
+        storage=atapi.AnnotationStorage(),
+        widget=atapi.StringWidget(
+            label=_(u"Reihennummer"),
+        ),
+    ),
+    atapi.StringField(
+        'isbn',
+        storage=atapi.AnnotationStorage(),
+        widget=atapi.StringWidget(
+            label=_(u"ISBN"),
+        ),
+    ),
+    atapi.StringField(
+        'ddcSach',
+        storage=atapi.AnnotationStorage(),
+        widget=atapi.StringWidget(
+            label=_(u"ddc sach"),
+        ),
+    ),
+    # Q: DateTimeField ?
+    atapi.StringField(
+        'ddcZeit',
+        storage=atapi.AnnotationStorage(),
+        widget=atapi.StringWidget(
+            label=_(u"ddc zeit"),
+        ),
+    ),
+    atapi.StringField(
+        'schlagwoerter',
+        storage=atapi.AnnotationStorage(),
+        widget=atapi.StringWidget(
+            label=_(u"Schlagwörter"),
+        ),
+    ),
 ))
 
 # Set storage on fields copied from ATContentTypeSchema, making sure
