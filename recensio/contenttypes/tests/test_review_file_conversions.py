63052ceb454a2fbe164f12df3885f920359987ae
deroiste <deroiste@51ee9ba4-e84d-4896-84de-354f1821c169>
2010-08-23 13:36:13 +0000

    Testing file conversions :doc, pdf and swf attached to reviews
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.contenttypes/trunk@5991 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/contenttypes/tests/test_review_file_conversions.py
+++ recensio/contenttypes/tests/test_review_file_conversions.py
@@ -5,13 +5,17 @@ these tests cover the conversions on those files to other formats.
 """
 import datetime
 from os import fstat
-
 import unittest2 as unittest
-from recensio.policy.tests.layer import RECENSIO_INTEGRATION_TESTING
+
+import zope.event
+
+from Testing import makerequest
 from plone.app.blob.utils import openBlob
+from Products.Archetypes.event import ObjectEditedEvent
 
 from wc.pageturner.settings import Settings
 
+from recensio.policy.tests.layer import RECENSIO_INTEGRATION_TESTING
 
 class TestReviewFileConversions(unittest.TestCase):
     """Test file conversions.
@@ -21,19 +25,42 @@ class TestReviewFileConversions(unittest.TestCase):
     """
     layer = RECENSIO_INTEGRATION_TESTING
 
-    def test_conversions(self):
+    def test_review_with_custom_pdf_files(self):
+        """Reviews / Presentations which have a custom pdf file will
+        use that version for downloading and conversion to swf
+        """
         portal = self.layer["portal"]
         sample_reviews = portal["sample-reviews"]
         review = sample_reviews[sample_reviews.objectIds()[0]]
-        # The sample reviews have pdf and word doc
+        # The sample reviews have both a pdf and a word doc attached
         self.assertTrue(review.doc.get_size() > 0)
         self.assertTrue(review.pdf.get_size() > 0)
         # Since there is a custom pdf, a pdf has not been generated
         self.assertFalse(hasattr(review, "generatedPdf"))
+        # An swf file has been created
         settings = Settings(review)
         swf_blob = openBlob(settings.data)
         swf_size = fstat(swf_blob.fileno()).st_size
         swf_blob.close()
         self.assertTrue(swf_size > 0)
-        pass
+        # get_review_pdf returns the custom pdf
+        self.assertTrue(review.pdf.blob == review.get_review_pdf())
 
+        # Remove the custom pdf and trigger the ObjectEditedEvent
+        # This should cause the pdf to be generated from the Word doc
+        # and the swf to be generated from that
+        review.setPdf(None)
+        self.assertTrue(review.getPdf().get_size() == 0)
+        request = makerequest.makerequest(review)
+        event = ObjectEditedEvent(review, request)
+        zope.event.notify(event)
+        self.assertTrue(hasattr(review, "generatedPdf"))
+        settings = Settings(review)
+        doc_swf_blob = openBlob(settings.data)
+        doc_swf_size = fstat(doc_swf_blob.fileno()).st_size
+        doc_swf_blob.close()
+        self.assertTrue(doc_swf_size != swf_size)
+
+        # TODO
+        # Remove the Word doc and the review text content should be
+        # used instead to create the pdf and swf versions
