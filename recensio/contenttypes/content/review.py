cf7102eb22db2de318145ceb6c00955535960f02
gerken <gerken@51ee9ba4-e84d-4896-84de-354f1821c169>
2010-10-20 11:49:17 +0000

    Doh, renamed variable, but not everywhere
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.contenttypes/trunk@6757 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/contenttypes/content/review.py
+++ recensio/contenttypes/content/review.py
@@ -224,12 +224,12 @@ class BaseReview(base.ATCTMixin, HistoryAwareMixin, atapi.BaseContent):
             return None
         retval = []
         for author in self.getAuthors():
-            authors_list.append(u'%s %s' % (author['firstname'].decode('utf8'), author['lastname'].decode('utf8')))
+            retval.append(u'%s %s' % (author['firstname'].decode('utf8'), author['lastname'].decode('utf8')))
         if listEditors:
             if not getattr(self, 'getEditorsCollectedEdition', None):
-                return authors_list
+                return retval
             for editor in self.getEditorsCollectedEdition():
-                authors_list.append(u'%s %s' % (editor['firstname'].decode('utf8'), editor['lastname'].decode('utf8')))
+                retval.append(u'%s %s' % (editor['firstname'].decode('utf8'), editor['lastname'].decode('utf8')))
         return retval
 
 
