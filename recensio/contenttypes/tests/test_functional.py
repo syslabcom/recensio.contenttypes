5b43fc2ec9701647aa340c9c2637444a90dd9b77
deroiste <deroiste@51ee9ba4-e84d-4896-84de-354f1821c169>
2010-11-17 12:58:55 +0000

    Deleting the test object afterwards to avoid breaking other tests.
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.contenttypes/trunk@6993 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/contenttypes/tests/test_functional.py
+++ recensio/contenttypes/tests/test_functional.py
@@ -7,6 +7,7 @@ import os
 import unittest2 as unittest
 
 import zope.event
+import transaction
 
 from plone.testing.z2 import Browser
 from Testing import makerequest
@@ -46,13 +47,14 @@ class TestCoverPicture(unittest.TestCase):
         browser.getControl("Presentation Monograph").selected = True
         browser.getControl("Add").click()
 
-        browser.getControl(name="title").value = "Test monograph"
+        browser.getControl(name="title").value = "Test Cover Pic"
         browser.getControl(name="reviewAuthorLastname").value = "Last"
         browser.getControl(name="reviewAuthorFirstname").value = "First"
         browser.getControl(name="reviewAuthorEmail").value = "e@mail.com"
         browser.getControl("Licence Agreement").selected = True
         browser.getControl("Save").click()
 
+        presentation_url = browser.url
         edit_url = browser.url+"/edit"
         browser.open(edit_url)
         img = os.path.dirname(__file__)+"/test.jpg"
@@ -66,3 +68,12 @@ class TestCoverPicture(unittest.TestCase):
         browser.getControl("Save").click()
         browser.open(edit_url)
         self.assertTrue("Current image" in browser.contents)
+
+        # Clean up:
+        # The workflow doesn't permit Presentations to be deleted so
+        # we need to do it directly
+        parent_folder = portal.unrestrictedTraverse(
+            "/plone/Members/test_user_1_")
+        parent_folder["test-cover-pic"].wl_clearLocks()
+        parent_folder.manage_delObjects("test-cover-pic")
+        transaction.commit()
