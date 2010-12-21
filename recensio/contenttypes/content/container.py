72f4c5ecb581f8ce4339d9d55b08baa7f7f8247e
thomas <thomas@51ee9ba4-e84d-4896-84de-354f1821c169>
2010-12-21 12:04:50 +0000

    make sure ITranslatable is not implemented by our Language-neutral containers (such as publications). Otherwise it is impossible to translate a Page (e.g. index_html) inside them, since LinguaPlone will try to translate the container (publication) as well
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.contenttypes/trunk@7462 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/contenttypes/content/container.py
+++ recensio/contenttypes/content/container.py
@@ -1,7 +1,10 @@
-from Products.ATContentTypes.content import folder
+from plone.app.folder import folder
+
 
 class Container(folder.ATFolder):
-    """ A class that extends ATFolder but acts language-neutral """
+    """ A class that extends ATFolder but acts language-neutral.
+    We make sure, by subclassing plone.app.folder's base class and
+    not ATContentType's, that ITranslatable is not implemented"""
 
     def Language(self):
         """ This content is neutral """
