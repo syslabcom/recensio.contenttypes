f15a00ffb704da8b44e53cead1e0c9faef52bcba
bauer <bauer@51ee9ba4-e84d-4896-84de-354f1821c169>
2011-08-08 15:41:49 +0000

    Adding failing test to demonstrate problem. refs #3127
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.contenttypes/trunk@8847 51ee9ba4-e84d-4896-84de-354f1821c169

---
+++ recensio/contenttypes/tests/testReviewView.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import unittest2 as unittest
+from mock import Mock
+
+class TestExcelImportUnit(unittest.TestCase):
+    def test_list_rows_empty(self):
+        from recensio.contenttypes.browser.review import View
+        view = View(None, None)
+        self.assertEquals("", view.list_rows([], "lastname", "firstname"))
+
+    def test_list_rows_stupid_empty(self):
+        from recensio.contenttypes.browser.review import View
+        view = View(None, None)
+        self.assertEquals("", view.list_rows([{"lastname":"", "firstname":""}], "lastname", "firstname"))
