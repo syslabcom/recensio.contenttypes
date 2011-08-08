07d48259b01d605d154d82fa798b0494edb7e265
bauer <bauer@51ee9ba4-e84d-4896-84de-354f1821c169>
2011-08-08 15:59:25 +0000

    Another failing test, if a there are empty elements in a multivalue field of archetypes. refs #3127
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.contenttypes/trunk@8849 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/contenttypes/tests/testReviewView.py
+++ recensio/contenttypes/tests/testReviewView.py
@@ -13,3 +13,8 @@ class TestExcelImportUnit(unittest.TestCase):
         from recensio.contenttypes.browser.review import View
         view = View(None, None)
         self.assertEquals("", view.list_rows([{"lastname":"", "firstname":""}], "lastname", "firstname"))
+
+    def test_list_rows_stupid_empty2(self):
+        from recensio.contenttypes.browser.review import View
+        view = View(None, None)
+        self.assertEquals("<ul class='rows_list'><li>a, b</li></ul>", view.list_rows([{"lastname":"a", "firstname":"b"},{"lastname":"", "firstname":""}], "lastname", "firstname"))
