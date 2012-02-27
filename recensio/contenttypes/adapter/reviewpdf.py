28f330037d4aece9eb717c5d7edb286912842d56
pilz <pilz@51ee9ba4-e84d-4896-84de-354f1821c169>
2012-02-27 15:12:14 +0000

    made pdf conversion async
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.contenttypes/trunk@9903 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/contenttypes/adapter/reviewpdf.py
+++ recensio/contenttypes/adapter/reviewpdf.py
@@ -7,6 +7,7 @@ import tempfile
 from zope import interface
 from zope import component
 
+from plone.app.async.interfaces import IAsyncService
 from plone.app.blob.field import ImageField
 
 from recensio.contenttypes import interfaces
@@ -15,6 +16,78 @@ from recensio.contenttypes.helperutilities import SubprocessException
 
 logger = logging.getLogger('recensio.contenttypes.adapter.reviewpdf.py')
 
+def _getAllPageImages(context, size=(320,452)):
+    # Get the pdf
+    pdf = context.get_review_pdf()
+    if pdf:
+        pdf_data = pdf["blob"].open().read()
+    if not pdf or not pdf_data:
+        return "%s has no pdf" %(
+            context.absolute_url), None
+    else:
+        # Split the pdf, one file per page
+        split_pdf_pages = RunSubprocess(
+            "pdftk",
+            output_params="burst output")
+        split_pdf_pages.create_tmp_input(suffix=".pdf", data=pdf_data)
+        split_pdf_pages.create_tmp_output_dir()
+        split_pdf_pages.output_path = os.path.join(
+            split_pdf_pages.tmp_output_dir,
+            "%04d.pdf")
+        split_pdf_pages.run()
+
+        msg = tuple()
+        if split_pdf_pages.errors != "":
+            msg += ("Message from split_pdf_pages:"
+                   "\n%s\n" % split_pdf_pages.errors,)
+
+        # Convert the pages to .gifs
+        # rewritten to have one converter step per page as we have seen process 
+        # sizes larger than 2GB for 60 pages in a batch
+        for filename in glob.glob(split_pdf_pages.tmp_output_dir+"/*.pdf"):
+            pdf_to_image = RunSubprocess(
+                "convert",
+                input_params="-density 250",
+                input_path=filename,
+                output_params="-resize %sx%s" % (size[0], size[1]))
+            outputname = '.'.join(filename.split("/")[-1].split('.')[:-1])+'.gif'
+            pdf_to_image.output_path = os.path.join(
+                split_pdf_pages.tmp_output_dir,
+                outputname)
+            pdf_to_image.run()
+            if pdf_to_image.errors != "":
+                msg += ("Message from pdfs_to_images:"
+                       "\n%s\n" % pdf_to_image.errors,)
+                
+            pdf_to_image.clean_up()
+
+        imgfiles = [gif for gif
+                    in os.listdir(split_pdf_pages.tmp_output_dir)
+                    if os.path.splitext(gif)[1] == '.gif']
+        imgfiles.sort()
+
+        pages = []
+        for img in imgfiles:
+            img = open(os.path.join(
+                    split_pdf_pages.tmp_output_dir, img),
+                         "r")
+            img_data = img.read()
+            pages.append(img_data)
+            img.close()
+
+        # Remove temporary files
+        split_pdf_pages.clean_up()
+
+        if pages:
+            imgfields = []
+            for img in pages:
+                IF = ImageField()
+                IF.set(context, img)
+                imgfields.append(IF)
+            setattr(context, 'pagePictures', imgfields)
+            
+        return msg
+        
 class ReviewPDF(object):
     """ Adapter to generate a cover image from a review's PDF data
     """
@@ -29,85 +102,7 @@ class ReviewPDF(object):
             self.__class__.__name__, self.context.Title())
     __repr__ = __str__
 
-    def _getAllPageImages(self, size=(320,452)):
-        # Get the pdf
-        pdf = self.context.get_review_pdf()
-        if pdf:
-            pdf_data = pdf["blob"].open().read()
-        if not pdf or not pdf_data:
-            return "%s has no pdf" %(
-                self.context.absolute_url), None
-        else:
-            # Split the pdf, one file per page
-            split_pdf_pages = RunSubprocess(
-                "pdftk",
-                output_params="burst output")
-            split_pdf_pages.create_tmp_input(suffix=".pdf", data=pdf_data)
-            split_pdf_pages.create_tmp_output_dir()
-            split_pdf_pages.output_path = os.path.join(
-                split_pdf_pages.tmp_output_dir,
-                "%04d.pdf")
-            split_pdf_pages.run()
 
-            msg = tuple()
-            if split_pdf_pages.errors != "":
-                msg += ("Message from split_pdf_pages:"
-                       "\n%s\n" % split_pdf_pages.errors,)
-
-            # Convert the pages to .gifs
-            # rewritten to have one converter step per page as we have seen process 
-            # sizes larger than 2GB for 60 pages in a batch
-            for filename in glob.glob(split_pdf_pages.tmp_output_dir+"/*.pdf"):
-                pdf_to_image = RunSubprocess(
-                    "convert",
-                    input_params="-density 250",
-                    input_path=filename,
-                    output_params="-resize %sx%s" % (size[0], size[1]))
-                outputname = '.'.join(filename.split("/")[-1].split('.')[:-1])+'.gif'
-                pdf_to_image.output_path = os.path.join(
-                    split_pdf_pages.tmp_output_dir,
-                    outputname)
-                pdf_to_image.run()
-                if pdf_to_image.errors != "":
-                    msg += ("Message from pdfs_to_images:"
-                           "\n%s\n" % pdf_to_image.errors,)
-                    
-                pdf_to_image.clean_up()
-
-            
-#            pdfs_to_images = RunSubprocess(
-#                "convert",
-#                input_params="-density 250",
-#                input_path=split_pdf_pages.tmp_output_dir+"/*.pdf",
-#                output_params="-resize %sx%s" % (size[0], size[1]))
-#            pdfs_to_images.output_path = os.path.join(
-#                split_pdf_pages.tmp_output_dir,
-#                "%04d.gif")
-#            pdfs_to_images.run()
-
-            imgfiles = [gif for gif
-                        in os.listdir(split_pdf_pages.tmp_output_dir)
-                        if os.path.splitext(gif)[1] == '.gif']
-            imgfiles.sort()
-
-            pages = []
-            for img in imgfiles:
-                img = open(os.path.join(
-                        split_pdf_pages.tmp_output_dir, img),
-                             "r")
-                img_data = img.read()
-                pages.append(img_data)
-                img.close()
-
-#            if pdfs_to_images.errors != "":
-#                msg = ("Message from pdfs_to_images:"
-#                       "\n%s\n" % pdfs_to_images.errors)
-
-            # Remove temporary files
-            split_pdf_pages.clean_up()
-#            pdfs_to_images.clean_up()
-
-            return msg, pages
 
     def generatePageImages(self):
         """
@@ -117,20 +112,23 @@ class ReviewPDF(object):
         i = 1
         pages = []
         status = 1
-        try:
-            result, pageimages = self._getAllPageImages((800,1131))
-        except SubprocessException, e:
-            result = "Missing converter? -> " + str(e)
-            pageimages = None
+        # make this asyncronous
+        async = component.getUtility(IAsyncService)
+        job = async.queueJob(_getAllPageImages, self.context, (800,1131))
+#        try:
+#            result, pageimages = self._getAllPageImages((800,1131))
+#        except SubprocessException, e:
+#            result = "Missing converter? -> " + str(e)
+#            pageimages = None
         if result:
             logger.warn("popen: %s" % (result))
             if 'Error:' in result:
                 status = 0
-        if pageimages:
-            imgfields = []
-            for img in pageimages:
-                IF = ImageField()
-                IF.set(self.context, img)
-                imgfields.append(IF)
-            setattr(self.context, 'pagePictures', imgfields)
+#        if pageimages:
+#            imgfields = []
+#            for img in pageimages:
+#                IF = ImageField()
+#                IF.set(self.context, img)
+#                imgfields.append(IF)
+#            setattr(self.context, 'pagePictures', imgfields)
         return status
