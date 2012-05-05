8a206c26224512f6a9c34f4ab525f7ef3f572216
deroiste <deroiste@51ee9ba4-e84d-4896-84de-354f1821c169>
2012-05-05 21:35:37 +0000

    Back to development: 2.9
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.contenttypes/trunk@9942 51ee9ba4-e84d-4896-84de-354f1821c169

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '2.8'
+version = '2.9.dev0'
 
 long_description = (
     read('README.txt')
