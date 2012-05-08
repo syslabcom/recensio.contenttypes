a7ac93627248bc969521dfe4ff6c85d6a9ea9eb8
deroiste <deroiste@51ee9ba4-e84d-4896-84de-354f1821c169>
2012-05-08 17:20:13 +0000

    Adding external script to recreate preview images
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.contenttypes/trunk@9954 51ee9ba4-e84d-4896-84de-354f1821c169

---
+++ recensio/contenttypes/Extensions/regenerate_pdfs_and_images.py
@@ -0,0 +1,27 @@
+from zope.app.component.hooks import getSite
+from DateTime import DateTime
+from recensio.contenttypes.adapter import reviewpdf
+
+portal = getSite()
+pc = portal.portal_catalog
+
+rev_types = [
+    "Review Monograph", "Presentation Monograph",
+    "Presentation Collection", "Presentation Article Review",
+    "Presentation Online Resource", "Review Journal" ]
+
+feb_brains = [
+    i for i in pc.searchResults({
+            "portal_type": rev_types,
+            "modified" : {
+                "query": [DateTime(2012,2,7)],
+                "range": "min", "sort_on" : "modified", "sort_order": "reverse"
+                }
+            })]
+
+def main():
+    for brain in feb_brains:
+        obj = brain.getObject()
+        obj.update_generated_pdf()
+        print reviewpdf._getAllPageImages(obj, (800,1131))
+
