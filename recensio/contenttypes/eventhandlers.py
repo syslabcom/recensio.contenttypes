3f1640aa5def7650afb487b7285ee4c29bcef4dc
gerken <gerken@51ee9ba4-e84d-4896-84de-354f1821c169>
2012-06-19 13:16:39 +0000

    Remove notification of new authors on edited reviews. Refs #4588
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.contenttypes/trunk@10025 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/contenttypes/eventhandlers.py
+++ recensio/contenttypes/eventhandlers.py
@@ -12,32 +12,3 @@ def review_pdf_updated_eventhandler(obj, evt):
     if not obj.REQUEST.get('pdf_file'):
         obj.update_generated_pdf()
 
-    interfaces.IReviewPDF(obj).generatePageImages()
-
-def notify_reference_authors_if_changed(obj, evt):
-    """Check if reference authors have been changed, if yes: notify added ones"""
-    rtool = getToolByName(obj, 'portal_repository')
-    wftool = getToolByName(obj, 'portal_workflow')
-
-    if not wftool.getInfoFor(obj, 'review_state') == 'published':
-        return
-
-    skip = []
-    try:
-        history = rtool.getHistory(obj)
-        if len(history) > 1:
-            prev_version = history[0].object
-            for refauth in obj.referenceAuthors:
-                if refauth['email'] in map(lambda x: x['email'], prev_version.referenceAuthors):
-                    mail = refauth.get('email', None)
-                    if mail:
-                        log.debug('skipping %s' % mail)
-                        skip.append(mail)
-    except:
-        # XXX Temporary only
-        log.warning('No previous object version found. Not comparing reference authors')
-    log.warning("Here are the ignored users: %s" % str(skip))
-
-    notify_view = obj.restrictedTraverse('@@mail_new_presentation')
-    if notify_view:
-        notify_view(skip_addrs=skip)
