8fc273524d5223e843170160a8ea405be0e51c89
Philip Bauer <bauer@starzel.de>
2012-10-23 16:28:35 +0200

    fix permission remove paster-nonsense

--- setup.py
+++ setup.py
@@ -77,6 +77,4 @@ setup(name='recensio.contenttypes',
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["ZopeSkel"],
       )
