a308dd1c08895722dcda08bb43012b9d53f8f89e
Patrick Gerken <do3cc@patrick-gerken.de>
2013-03-28 17:33:16 +0100

    Lost retval

--- recensio/contenttypes/content/review.py
+++ recensio/contenttypes/content/review.py
@@ -343,6 +343,7 @@ class BaseReview(base.ATCTMixin, HistoryAwareMixin, atapi.BaseContent):
             u' / ', ', ', self.reviewAuthors, lastname_first=True)
         if review_author.strip() != ',':
             retval.append(safe_unicode(review_author).encode('utf-8'))
+        return retval
 
     def getAllAuthorDataFulltext(self):
         authors = " ".join(self.getAllAuthorData())
