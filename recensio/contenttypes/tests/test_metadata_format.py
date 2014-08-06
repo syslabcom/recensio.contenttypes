eafcf6eba97d8fcced5398c2b2e4443c8c37d589
Cillian de Róiste <goibhniu@fsfe.org>
2014-08-06 13:04:02 +0200

    Refs #10481 add tests for existing get_citation_string, before refactoring

--- recensio/contenttypes/tests/test_metadata_format.py
+++ recensio/contenttypes/tests/test_metadata_format.py
@@ -20,6 +20,7 @@ class TestMetadataFormat(unittest.TestCase):
 
         self.issue = self.portal["sample-reviews"]["newspapera"]["summer"]["issue-2"]
         login(self.layer['app'], SITE_OWNER_NAME)
+        self.maxDiff = None
 
     def test_review_monograph_decorated_title(self):
         item_id = "rm1"
@@ -132,3 +133,135 @@ class TestMetadataFormat(unittest.TestCase):
             u'et sociales (präsentiert von Delphine Cavallo)'
         )
         self.assertEqual(correct_title, generated_title)
+
+    def test_review_citation_string_title(self):
+        item_id = "rm1"
+        self.issue.invokeFactory(
+            'Review Monograph',
+            id=item_id,
+            title='Tristano e Isotta',
+            yearOfPublication="2013",
+            reviewAuthors=({'firstname': 'Margherita', 'lastname': 'Lecco'},),
+            authors=({'firstname': '', 'lastname': u'Béroul'},),
+            editorial=({'firstname': 'Gioia', 'lastname': 'Paradisi'},),
+        )
+        item = self.issue[item_id]
+        uid = item.UID()
+        generated_citation_string = item.get_citation_string()
+        correct_title = (
+            u'Lecco, Margherita: Rezension über: Gioia Paradisi (Hg.): Béroul, Tristano e Isotta, '
+            u'2013, in: Zeitschrift 1, Summer, Issue 2, <a href="http://nohost/plone/r/%s">'
+            u'http://nohost/plone/r/%s</a>' % (uid, uid)
+        )
+        self.assertEqual(correct_title, generated_citation_string)
+
+    def test_reviewjournal_citation_string(self):
+        item_id = "rj1"
+        self.issue.invokeFactory(
+            'Review Journal',
+            id=item_id,
+            title='Plone Mag',
+            reviewAuthors=({'firstname': 'Cillian', 'lastname': 'de Róiste'},),
+            yearOfPublication="2009",
+            officialYearOfPublication="2010",
+            volumeNumber="1",
+            issueNumber="3",
+        )
+        item = self.issue[item_id]
+        uid = item.UID()
+        generated_citation_string = item.get_citation_string()
+        correct_title = (
+            u'de Róiste, Cillian: Rezension über: Plone Mag, 1, 3 (2010/2009), in: Zeitschrift 1, '
+            u'Summer, Issue 2, <a href="http://nohost/plone/r/%s">http://nohost/plone/r/%s</a>' %
+            (uid, uid)
+        )
+        self.assertEqual(correct_title, generated_citation_string)
+
+    def test_presentationarticlereview_citation_string(self):
+        member_folder = self.portal.Members.fake_member
+        item_id = "par1"
+        member_folder.invokeFactory(
+            'Presentation Article Review',
+            id=item_id,
+            title=u'À la recherche d’une paix de compromis',
+            subtitle=u'Kessler, Haguenin et la diplomatie secrète de l’hiver 1916-1917',
+            authors=({'firstname': 'Landry', 'lastname': 'Charrier'},),
+            reviewAuthors=({'firstname': 'Landry', 'lastname': 'Charrier'},),
+            yearOfPublication="2010",
+            volumeNumber="11",
+        )
+        item = member_folder[item_id]
+        uid = item.UID()
+        generated_citation_string = item.get_citation_string()
+        correct_title = (
+            u'Charrier, Landry: Präsentation von: Charrier, Landry, À la recherche d’une paix de '
+            u'compromis. Kessler, Haguenin et la diplomatie secrète de l’hiver 1916-1917, in: 11 '
+            u'<a href="http://nohost/plone/r/%s">http://nohost/plone/r/%s</a>' % (uid, uid)
+        )
+        self.assertEqual(correct_title, generated_citation_string)
+
+    def test_presentationcollection_citation_string(self):
+        member_folder = self.portal.Members.fake_member
+        item_id = "pc1"
+        member_folder.invokeFactory(
+            'Presentation Collection',
+            id=item_id,
+            title=u'Christmas Truce',
+            subtitle=u'Die Amateurfotos vom Weihnachtsfrieden 1914 und ihre Karriere',
+            authors=({'firstname': 'Christian', 'lastname': ' Bunnenberg'},),
+            reviewAuthors=({'firstname': 'Christian', 'lastname': 'Bunnenberg'},),
+            yearOfPublication="2009",
+        )
+        item = member_folder[item_id]
+        uid = item.UID()
+        generated_citation_string = item.get_citation_string()
+        correct_title = (
+            u'Bunnenberg, Christian: Präsentation von: Bunnenberg, Christian, Christmas Truce. Die '
+            u'Amateurfotos vom Weihnachtsfrieden 1914 und ihre Karriere, in: 2009 '
+            u'<a href="http://nohost/plone/r/%s">http://nohost/plone/r/%s</a>' % (uid, uid)
+        )
+        self.assertEqual(correct_title, generated_citation_string)
+
+    def test_presentationmonograph_citation_string(self):
+        member_folder = self.portal.Members.fake_member
+        item_id = "pm1"
+        member_folder.invokeFactory(
+            'Presentation Monograph',
+            id=item_id,
+            title=u'Gelebter Internationalismus',
+            subtitle=u'Österreichs Linke und der algerische Widerstand (1958-1963)',
+            authors=({'firstname': 'Fritz', 'lastname': ' Keller'},),
+            reviewAuthors=({'firstname': 'Fritz', 'lastname': 'Keller'},),
+            yearOfPublication="2009",
+        )
+        item = member_folder[item_id]
+        uid = item.UID()
+        generated_citation_string = item.get_citation_string()
+        correct_title = (
+            u'Keller, Fritz: Präsentation von: Fritz Keller, Gelebter Internationalismus. '
+            u'Österreichs Linke und der algerische Widerstand (1958-1963), 2009, '
+            u'<a href="http://nohost/plone/r/%s">http://nohost/plone/r/%s</a>' % (uid, uid)
+        )
+        self.assertEqual(correct_title, generated_citation_string)
+
+    def test_presentationonlineresource_citation_string(self):
+        member_folder = self.portal.Members.fake_member
+        item_id = "por1"
+        member_folder.invokeFactory(
+            'Presentation Online Resource',
+            id=item_id,
+            title=(
+                u'Revues.org, plateforme de revues et de collections de livres en sciences '
+                u'humaines et sociales'
+            ),
+            reviewAuthors=({'firstname': 'Delphine', 'lastname': 'Cavallo'},),
+        )
+        item = member_folder[item_id]
+        uid = item.UID()
+        generated_citation_string = item.get_citation_string()
+        correct_title = (
+            u'Cavallo, Delphine: Präsentation von: Revues.org, plateforme de revues et de '
+            u'collections de livres en sciences humaines et sociales, '
+            '<a href="http://nohost/plone/r/%s">http://nohost/plone/r/%s</a>' % (uid, uid)
+        )
+        self.assertEqual(correct_title, generated_citation_string)
