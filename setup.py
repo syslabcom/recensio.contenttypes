a341f432d79b33bfce522f0eee26e6cdcb597efd
Manuel Reinhardt <reinhardt@syslab.com>
2018-03-05 13:38:49 +0000

    Preparing release 4.4.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '4.3.3.dev0'
+version = '4.4.0'
 
 long_description = (
     read('README.txt')
