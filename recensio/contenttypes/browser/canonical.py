10ad4f1efc42c8ff36bc778a5c58084c92c4c5ec
Manuel Reinhardt <reinhardt@syslab.com>
2018-10-08 08:22:38 +0000

    Leave the URL alone if there's no virtual URL (mainly in a dev instance). Refs #16548

--- recensio/contenttypes/browser/canonical.py
+++ recensio/contenttypes/browser/canonical.py
@@ -8,7 +8,8 @@ class CanonicalURLHelper(object):
     def get_canonical_url(self):
         registry = getUtility(IRegistry)
         recensio_settings = registry.forInterface(IRecensioSettings)
-        if self.request['VIRTUAL_URL_PARTS'][0] != recensio_settings.external_portal_url:
+        virtual_url = self.request.get('VIRTUAL_URL_PARTS', [])
+        if virtual_url and virtual_url[0] != recensio_settings.external_portal_url:
             canonical_url = '/'.join((
                 recensio_settings.external_portal_url,
                 '/'.join(self.context.getPhysicalPath()[2:]),
