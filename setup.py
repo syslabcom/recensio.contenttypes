72e8cde19e18890b5454220af4df5c484281d335
Manuel Reinhardt <reinhardt@syslab.com>
2021-11-08 10:50:04 +0100

    Preparing release 4.8.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
-version = "4.8.1.dev0"
+version = "4.8.1"
 
 long_description = (
     read("README.txt") + "\n" + "Requirements\n"
