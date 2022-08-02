acf6ca3c962bfcf50600739087973de3ba7ad124
Manuel Reinhardt <reinhardt@syslab.com>
2022-08-02 08:17:00 +0200

    Back to development: 4.8.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
-version = "4.8.2"
+version = "4.8.3.dev0"
 
 long_description = (
     read("README.txt") + "\n" + "Requirements\n"
